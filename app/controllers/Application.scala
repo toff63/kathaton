9c2a35034cce3976c89b0671f4a36f9da84c6328
Marcio Oliveira <marciottlg@gmail.com>
2013-06-19 16:40:25 -0300

    hackathonForm with proper fields

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,6 +5,7 @@ import play.api.data._
 import play.api.data.Forms._
 import play.api.mvc.Action
 import play.api.mvc.Controller
+import java.util.Date
 
 object Application extends Controller {
 
@@ -16,16 +17,16 @@ object Application extends Controller {
     Ok(views.html.index(Hackathon.all(), hackathonForm))
   }
 
-//  def newHackathon = Action { implicit request =>
-//    hackathonForm.bindFromRequest.fold(
-//      errors => BadRequest(views.html.index(Hackathon.all(), hackathonForm)),
-//      label => {
-//        Hackathon.create(label)
-//        Redirect(routes.Application.hackathons)
-//      })
-//  }
+  def newHackathon = Action { implicit request =>
+    hackathonForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(Hackathon.all(), hackathonForm)),
+      project => {
+        Hackathon.create("", "", new Date())
+        Redirect(routes.Application.hackathons)
+      })
+  }
 
-  def deleteHackathon(id: Long) = TODO
+  def deleteHackathon(project: String) = TODO
 
     val hackathonForm = Form(
         mapping(
