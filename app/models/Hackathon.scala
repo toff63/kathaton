e852d08295f957c275d21004e2c356847b1fc501
Andre Mayer <mayer2004@hotmail.com>
2013-06-19 16:07:23 -0300

    go

--- app/models/Hackathon.scala
+++ app/models/Hackathon.scala
@@ -5,7 +5,11 @@ import java.util.Date
 case class Hackathon(id: Long, project: String, date: Date)
 
 object Hackathon {
-  def all(): List[Hackathon] = Nil
-  def create(name: String) {}
+  var hackatons:List[Hackathon] = List(new Hackathon(1,"", new Date))
+  
+  def all(): List[Hackathon] = hackatons
+  def create(name: String) { 
+    hackatons :+ new Hackathon(hackatons.size, name, new Date)
+    }
   def delete(id: Long) {}
 }
