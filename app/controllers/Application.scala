f605cb4b3c1d8b610266015e68f8ee5d83900516
Rafael Salerno <salerno.rafael@gmail.com>
2013-06-19 15:37:18 -0300

    fix for test

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-    Redirect(routes.Application.hackathons)
+    Ok("Hello world")
   }
   
   def hackathons = TODO
