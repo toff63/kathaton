a366855a9793846cf33586688143c3c3c566eb90
Christophe Marchal <toff63@gmail.com>
2013-06-19 18:29:20 -0300

    added mongodb configuration

--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,17 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "kathaton"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "kathaton"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
-  )
-
+    anorm,
+    "se.radley" %% "play-plugins-salat" % "1.2")
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+    routesImport += "se.radley.plugin.salat.Binders._",
+    templatesImport += "org.bson.types.ObjectId")
 
 }
