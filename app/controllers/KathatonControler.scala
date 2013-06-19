c1c8dc8438948d698aae0966b5c4f56f452cab0c
Jackson <cyber.jso@gmail.com>
2013-06-19 15:14:28 -0300

    Adding Kathaton controller

---
+++ app/controllers/KathatonControler.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Kathaton extends Controller {
+
+	def index  = Action {
+		Ok("cook more")
+	}
+	
+	def add =  TODO
+	
+	def list = TODO
+	
+	def delete = TODO
+	
+}
