8a4e7350ba9c522fc14c8a5716a336fb8c914db1
Rafael de Paula Souza <rafael.bnc@gmail.com>
2013-06-19 18:26:55 -0300

    rank with progress

--- app/controllers/IdeiasController.scala
+++ app/controllers/IdeiasController.scala
@@ -5,7 +5,7 @@ import models.Ideia
 
 object IdeiasController extends Controller {
 
-  var listaString = List(new Ideia(1, "bla", 5), new Ideia(1, "ble", 10), new Ideia(1, "bli", 5), new Ideia(1, "blo", 50))
+  var listaString = List(new Ideia(1, "bla", 25), new Ideia(1, "ble", 100), new Ideia(1, "bli", 5), new Ideia(1, "blo", 50))
 
   def list = Action {
     Ok(views.html.ideia(listaString))
