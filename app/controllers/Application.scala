96963daf4a8695c78b1b12ca44aee93b663a0b04
Andre Mayer <mayer2004@hotmail.com>
2013-06-19 15:39:39 -0300

    go

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,18 +1,25 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
+import models.Hackathon
+import play.api.data.Form
+import play.api.data.Forms.nonEmptyText
+import play.api.mvc.Action
+import play.api.mvc.Controller
 
 object Application extends Controller {
-  
+
   def index = Action {
     Redirect(routes.Application.hackathons)
   }
-  
-  def hackathons = TODO
-  
+
+  val hackatonForm = Form("label" -> nonEmptyText)
+
+  def hackathons = Action {
+    Ok(views.html.index(Hackathon.all(), hackatonForm))
+  }
+
   def newHackathon = TODO
-  
+
   def deleteHackathon(id: Long) = TODO
-  
+
 }
