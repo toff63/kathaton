ad3e4114c6b83b50fc06cd9f8fa406ff99397894
Leonardo <leo.poa@gmail.com>
2013-09-04 16:11:08 -0300

    add

--- app/controllers/GambeiroController.scala
+++ app/controllers/GambeiroController.scala
@@ -6,7 +6,7 @@ import models.Gambeiro
 
 object GambeiroController extends Controller {
 
-  var listaGambeiros = List(new Gambeiro(1, "Caopeao"))
+  var listaGambeiros = List(new Gambeiro(1, "Caopeao"), new Gambeiro(1, "Borges"), new Gambeiro(1, "Joel Santana"), new Gambeiro(1, "Bob Miner"))
 
   def list = Action {
     Ok(views.html.gambeiro(listaGambeiros))
