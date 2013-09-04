1fa0bad3fbef0f2e599ca8d18043abc4a77b572b
Jéferson Machado <jefersonm@gmail.com>
2013-09-04 15:52:52 -0300

    Improving language ranking

--- app/controllers/LanguagesController.scala
+++ app/controllers/LanguagesController.scala
@@ -8,6 +8,12 @@ object LanguagesController extends Controller {
   var lista = List(new Ideia(1, "Scala", 25), new Ideia(2, "Clojure", 50))
 
   def list = Action {
-    Ok(views.html.ideia(lista))
+    Ok(views.html.language(lista))
   }
+
+  def vote(id:Long) = Action{
+    lista.filter(id == _.id).map(_.rank +=10);
+    Ok(views.html.language(lista))
+  }
+
 }
